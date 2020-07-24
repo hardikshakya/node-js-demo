ccbbacc05bb897d1775f527e8a706eacb2ed820e
hardikshakya <hardikshakya1997@gmail.com>
2020-07-24 18:48:10 +0530

    P3T3: Data caching part 1 - Patching Mongoose's Exec - Restoring Blog Routes Handler - Unique keys - Key Creation - Restoring Redis Config - Cache Implementation - Resolving Values

---
+++ P3_Project/services/cache.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+const redis = require('redis');
+const util = require('util');
+
+const redisUrl = 'redis://127.0.0.1:6379'; // Your redis server URL
+const client = redis.createClient(redisUrl);
+client.get = util.promisify(client.get);
+const exec = mongoose.Query.prototype.exec;
+
+mongoose.Query.prototype.exec = async function () {  
+  const key = JSON.stringify(Object.assign({}, this.getQuery(), {
+    collection: this.mongooseCollection.name
+  }));
+
+  // See if we have a value for 'key' in redis
+  const cacheValue = await client.get(key);
+
+  // If we do, return that
+  if (cacheValue) {
+    console.log(cacheValue);
+    
+    return JSON.parse(cacheValue);
+  }
+
+  // Otherwise, issue the query and store the result in redis
+  const result = await exec.apply(this, arguments);
+
+  client.set(key, JSON.stringify(result));
+
+  return result;
+};
